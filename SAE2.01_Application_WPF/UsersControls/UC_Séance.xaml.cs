d57e9dade8add21b30974a7260bd2a5cb24704f8
soren <soren.thomas@etu.univ-smb.fr>
2026-06-04 18:23:10 +0200

    filtre cours 1 partie

--- SAE2.01_Application_WPF/UsersControls/UC_Séance.xaml.cs
+++ SAE2.01_Application_WPF/UsersControls/UC_Séance.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SAE2._01_Application_WPF.Classes;
 
 namespace SAE2._01_Application_WPF.UsersControls
 {
@@ -24,5 +26,40 @@ namespace SAE2._01_Application_WPF.UsersControls
         {
             InitializeComponent();
         }
+        private void FiltrerCategorie(object sender, RoutedEventArgs e)
+        {
+            string categorie = (sender as Button)?.Tag as string;
+
+            ICollectionView vue = CollectionViewSource.GetDefaultView(dgSeances.ItemsSource);
+            if (vue == null) return;
+
+            if (string.IsNullOrEmpty(categorie))
+                vue.Filter = null;   // "Toutes les catégories"
+            else
+                vue.Filter = obj =>
+                    (obj as Seance)?.UnCours.UneCategorie.NomCategorie == categorie;
+
+            MettreEnValeurBouton(sender as Button);   // surbrillance (optionnel)
+        }
+        private void MettreEnValeurBouton(Button actif)
+        {
+            BrushConverter bc = new BrushConverter();
+            foreach (var child in panelCategories.Children)
+            {
+                if (child is Button b)
+                {
+                    b.Background = Brushes.White;
+                    b.Foreground = (Brush)bc.ConvertFrom("#555555");
+                    b.BorderThickness = new Thickness(1);
+                }
+            }
+            if (actif != null)
+            {
+                actif.Background = (Brush)bc.ConvertFrom("#F26B1A");
+                actif.Foreground = Brushes.White;
+                actif.BorderThickness = new Thickness(0);
+            }
+        }
     }
 }
+
