590e83c63dd5accdbdc6a7bfffddef31106dc364
SawaHaki0 <Abdoul.Sawadogo@etu.univ-smb.fr>
2026-06-04 11:08:50 +0200

    Classe inscription?

---
+++ SAE2.01_Application_WPF/Classes/Inscription.cs
@@ -0,0 +1,79 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE2._01_Application_WPF.Classes
+{
+    internal class Inscription
+    {
+        private int idSeance, idClient;
+        private DateTime dateInscription;
+
+        public Inscription(int idSeance, int idClient, DateTime dateInscription)
+        {
+            this.IdSeance = idSeance;
+            this.IdClient = idClient;
+            this.DateInscription = dateInscription;
+        }
+
+        public int IdSeance
+        {
+            get
+            {
+                return this.idSeance;
+            }
+
+            set
+            {
+                this.idSeance = value;
+            }
+        }
+
+        public int IdClient
+        {
+            get
+            {
+                return this.idClient;
+            }
+
+            set
+            {
+                this.idClient = value;
+            }
+        }
+
+        public DateTime DateInscription
+        {
+            get
+            {
+                return this.dateInscription;
+            }
+
+            set
+            {
+                this.dateInscription = value;
+            }
+        }
+
+        public List<Inscription> FindAll()
+        {
+            List<Inscription> lesInscriptions = new List<Inscription>();
+            using (NpgsqlCommand cmdSelect = new NpgsqlCommand("select * from INSCRIPTION  ;"))
+            {
+                DataTable dt = DataAccess.ExecuteSelect(cmdSelect);
+                foreach (DataRow dr in dt.Rows)
+                    lesInscriptions.Add(new Inscription(
+                        (int)dr["SEANCE_ID"],
+                        (int)dr["CLIENT_ID"],
+                        ((DateOnly)dr["DATE_INSCRIPTION"]).ToDateTime(TimeOnly.MinValue)
+                    ));
+
+            }
+            return lesInscriptions;
+        }
+    }
+}
